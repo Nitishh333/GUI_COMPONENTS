cbeff7c19987371f5d6d787f0a6fd3ac96117cbf
KAVIYARASU-ASHOKAN <kaviyarasu266@gmail.com>
2024-08-02 19:37:20 +0530

    first commit

---
+++ app/src/main/java/com/example/gui_components/MainActivity.kt
@@ -0,0 +1,80 @@
+package com.example.gui_components
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Button
+import androidx.compose.material3.Label
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.OutlinedButton
+import androidx.compose.material3.OutlinedTextField
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.material3.TextField
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.focus.focusModifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import com.example.gui_components.ui.theme.GUI_COMPONENTSTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContent {
+              GUI()
+        }
+    }
+}
+
+@Preview
+@Composable
+fun GUI(){
+
+    Column {
+
+        Spacer(modifier = Modifier.height(50.dp))
+
+        Box(modifier = Modifier.padding(8.dp).fillMaxWidth()){
+            Text(text = "Hello world",
+                style = MaterialTheme.typography.displayMedium,
+                color = Color.Blue,
+                modifier=Modifier.align(Alignment.Center))
+        }
+
+        Spacer(modifier = Modifier.height(8.dp))
+
+        var name by remember { mutableStateOf("") }
+        OutlinedTextField(value =name ,
+            onValueChange ={name=it},
+            label = { Text(text = "NAME")},
+            modifier = Modifier.fillMaxWidth())
+
+        Spacer(modifier = Modifier.height(8.dp))
+
+        Box(modifier = Modifier.padding(8.dp).fillMaxWidth(), contentAlignment = Alignment.Center){
+            Button(onClick = { /* Do something */ }) {
+                Text("Click Me")
+        }
+
+        }
+
+        }
+    }
+
